1536eb16bdbd3c29d07390665e7424e308ad82d0
sinan artun <sinanartun@gmail.com>
2022-10-18 16:16:29 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import re
+import time
+import os.path
+
+start_time1 = time.time()
+regex1 = r"(\d{2})\.(\d{2})\.(\d{4})"
+regex2 = r"(\d+)\.(\d+)\.(\d+)"
+
+test_str = "03.27.2022"
+
+for x in range(10000000):
+    matches = re.search(regex1, test_str)
+end_time1 = time.time()
+result1 = end_time1 - start_time1
+
+print("Result 1 completed in: {:.6f} seconds".format(result1))
+
+start_time2 = time.time()
+
+for x in range(10000000):
+    matches = re.search(regex2, test_str)
+end_time2 = time.time()
+result2 = end_time2 - start_time2
+
+print("Result 2 completed in: {:.6f} seconds".format(result2))
+
+if result1 < result2:
+    print("first regex has greater performance " + regex1)
+else:
+    print("second regex has greater performance " + regex2)
